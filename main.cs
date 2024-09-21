311952df8377e0a277ff224fc2a87c0df564def7
XiomarRob <157074573+XiomarRob@users.noreply.github.com>
2024-09-21 00:57:53 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,40 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Enter wind speed in mph: ");
+
+    double windMph;
+    bool isMphValid = double.TryParse(Console.ReadLine(), out windMph);
+
+    if (isMphValid)
+    {
+      Console.WriteLine("Incorrect input. Enter a valid number");
+      return;
+      }
+    if (windMph >= 157)
+    {
+      Console.WriteLine("Category Five Hurricane");
+    }
+    if (windMph >= 130)
+    {
+      Console.WriteLine("Category Four Hurricane");
+    }
+    if (windMph >= 111)
+    {
+      Console.WriteLine("Category Three Hurricane");
+    }
+    if (windMph >= 96)
+    {
+      Console.WriteLine("Category Two Hurricane");
+    }
+    if (windMph >= 74)
+    {
+      Console.WriteLine("Category One Hurricane");
+    }
+    else if (windMph < 74)
+    {
+      Console.WriteLine("Not a hurricane");
+    }
+  }
+}
